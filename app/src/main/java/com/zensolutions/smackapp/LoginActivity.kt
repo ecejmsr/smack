effaeee3b1412dc7baa1896b0ef49ef02e3c4234
ecejmsr <ecejmsr>
2018-08-20 22:02:47 -0400

    Created Sign Up xml and activity

--- app/src/main/java/com/zensolutions/smackapp/LoginActivity.kt
+++ app/src/main/java/com/zensolutions/smackapp/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.zensolutions.smackapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun LoginCreateUserBtnCicked(view: View){
+        val signUpIntent= Intent(this, SignUpActivity::class.java)
+        startActivity(signUpIntent)
+
     }
 }
